21530dc3da589e8b94e45aa2b1b80ff5be066b0c
Tatyumi <s150204@nishitech.ac.jp>
2019-07-06 00:56:28 +0900

    新規追加

---
+++ Assets/Scripts/SkipController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Common;
+
+public class SkipController : MonoBehaviour
+{
+    /// <summary>オーディオマネージャー</summary>
+    private AudioManager audioManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左クリック押下判別
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 左クリックした場合
+
+            // 音停止
+            audioManager.StopSound();
+
+            // 集会シーンに遷移
+            SceneManager.LoadScene(SceneName.GATHER_SCENE);
+        }
+    }
+}
