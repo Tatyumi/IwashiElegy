751c1adca69be41e2ce2722146a1ff595f4549b5
Tatyumi <s150204@nishitech.ac.jp>
2019-07-06 22:35:37 +0900

    新規追加

---
+++ Assets/Scripts/ContinueController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Common;
+
+public class ContinueController : MonoBehaviour
+{
+    /// <summary>
+    /// ゲームを再開する
+    /// </summary>
+    public void Continue()
+    {
+        // ウェーブシーンに遷移
+        SceneManager.LoadScene(SceneName.WAVE_SCENE);
+    }
+
+    /// <summary>
+    /// タイトル画面に戻る
+    /// </summary>
+    public void GiveUp()
+    {
+        // タイトルシーンに遷移
+        SceneManager.LoadScene(SceneName.TITLE_SCENE);
+    }
+}
