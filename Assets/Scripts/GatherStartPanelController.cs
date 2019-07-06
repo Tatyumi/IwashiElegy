84c6d234720e6b5f7676c18ce04c0d041056f560
Tatyumi <s150204@nishitech.ac.jp>
2019-07-06 11:47:49 +0900

    集会開始処理の追加
    一定時間たつと、集会が開始するようにした。

--- Assets/Scripts/GatherStartPanelController.cs
+++ Assets/Scripts/GatherStartPanelController.cs
@@ -6,23 +6,46 @@ public class GatherStartPanelController : MonoBehaviour
 {
     /// <summary>全てのゲームオブジェクト</summary>
     public GameObject[] AllGameObjects;
+    /// <summary>経過時間</summary>
+    private float delta;
+    /// <summary>目標時間</summary>
+    private const float targetTime = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // 初期化処理
+        Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 時間計測
+        delta += Time.deltaTime;
 
+        // 目標時間経ったか判別
+        if(delta > targetTime)
+        {
+            // 経った場合
+
+            // 全てのゲームオブジェクトを表示にする
+            WaveDirector.GameObjectsSwtichActive(AllGameObjects, true);
+
+            // 集会開始パネルを非表示にする
+            gameObject.SetActive(false);
+        }
     }
 
-
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
     private void Initialize()
     {
         // 全てのゲームオブジェクトを非表示にする
         WaveDirector.GameObjectsSwtichActive(AllGameObjects, false);
+
+        // 初期値代入
+        delta = 0.0f;
     }
 }
