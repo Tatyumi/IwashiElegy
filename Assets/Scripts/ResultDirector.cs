e69a2c64620c21b953c8b687749dc69cf4e78d5b
Tatyumi <s150204@nishitech.ac.jp>
2019-07-06 12:09:39 +0900

    新規追加

---
+++ Assets/Scripts/ResultDirector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultDirector : MonoBehaviour
+{
+    /// <summary>結果フラグ</summary>
+    public static bool isResult;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 判定の取得
+    // trueなら勝利、falseなら敗北
+}
