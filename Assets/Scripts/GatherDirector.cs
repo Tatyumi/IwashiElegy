1c613ac5e4eaa5126fb1be25c7441f2de4c0c543
Tatyumi <s150204@nishitech.ac.jp>
2019-07-06 20:47:11 +0900

    新規追加

---
+++ Assets/Scripts/GatherDirector.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatherDirector : MonoBehaviour
+{
+    /// <summary>全てのゲームオブジェクト</summary>
+    public GameObject[] AllGameObjects;
+    /// <summary>スタートカンバスオブジェクト</summary>
+    public GameObject StartCanvas;
+    /// <summary>目標時間</summary>
+    private const float targetTime = 3.0f;
+    /// <summary>オーディオマネージャー</summary>
+    private AudioManager audioManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // 初期化処理
+        Initialize();
+
+        // コルーチン取得
+        var corutine = StartGame();
+
+        // コルーチン開始
+        StartCoroutine(corutine);
+    }
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    private void Initialize()
+    {
+        // 全てのゲームオブジェクトを非表示にする
+        WaveDirector.GameObjectsSwtichActive(AllGameObjects, false);
+
+        // 表示
+        StartCanvas.SetActive(true);
+
+        // 音楽データ取得
+        audioManager = AudioManager.Instance;
+    }
+
+
+    private IEnumerator StartGame()
+    {
+        // TODO 開始SE
+
+        // 待機時間
+        var wait = new WaitForSeconds(2);
+
+        yield return wait;
+
+        // BGM再生
+        audioManager.PlayBGM(audioManager.GatherBGM.name);
+
+        // 全てのゲームオブジェクトを表示
+        WaveDirector.GameObjectsSwtichActive(AllGameObjects, true);
+
+        // 非表示
+        StartCanvas.SetActive(false);
+    }
+}
