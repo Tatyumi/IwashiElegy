e6f43f673744ff0e8257a69c822907cf04c30cf2
Tatyumi <s150204@nishitech.ac.jp>
2019-07-07 14:36:47 +0900

    新規追加

---
+++ Assets/Scripts/OjamaController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OjamaController : MonoBehaviour, IApplableScore
+{
+    /// <summary>お邪魔キャラのデータ</summary>
+    public OjamaData OjamaData;
+    /// <summary>オーディオマネージャー</summary>
+    private AudioManager audioManager;
+
+    void Start()
+    {
+        // 初期化
+        Initialize();
+    }
+
+    void Update()
+    {
+        // ゲームが終了しているか判別
+        if (!TimeLimtController.isEnd)
+        {
+            // 終了していない場合
+
+            // 移動処理
+            transform.Translate(OjamaData.MoveSpeed);
+        }
+    }
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    private void Initialize()
+    {
+        // 音楽データ取得
+        audioManager = AudioManager.Instance;
+    }
+
+    /// <summary>
+    /// スコア適応処理
+    /// </summary>
+    public void ApplyScore()
+    {
+        // SE再生
+        audioManager.PlaySE(audioManager.DamageSE.name);
+
+        // いわしの数を減らす
+        IwashiCountController.SubtractIwashi(OjamaData.KillIwashiCount, OjamaData.MeterDamage);
+
+        // 破棄
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/OjamaData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// アセットメニューに追加
+[CreateAssetMenu(menuName = "MyScriptable/Create OjamaData")]
+
+public class OjamaData : ScriptableObject
+{
+    /// <summary>いわし撃破数</summary>
+    public int KillIwashiCount;
+    /// <summary>移動速度</summary>
+    public Vector2 MoveSpeed;
+    /// <summary>メーターダメージ</summary>
+    public float MeterDamage;
+
+}
